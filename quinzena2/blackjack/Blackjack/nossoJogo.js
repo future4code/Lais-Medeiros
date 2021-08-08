c878750754e85ce57d5217f20f4f9b657f2737ea
laisMedeiros <medeiros_online@yahoo.com.br>
2021-08-07 22:41:53 -0300

    Quinzena2 projeto

---
+++ quinzena2/blackjack/Blackjack/nossoJogo.js
@@ -0,0 +1,65 @@
+/**
+ * EXEMPLO DE UTILIZAÇÃO DA 'comprarCarta'
+ * 
+ * 
+    const carta = comprarCarta(); // Sorteia uma carta. Por exemplo, o rei de ouros
+    
+    console.log(carta.texto) // imprime o texto da carta. Exemplo: "K♦️" (indica "K" de ouros)
+    console.log(carta.valor) // imprime o valor da carta (um número). Exemplo: 10 (dado que "K" vale 10)
+ * 
+ * 
+ * 
+ */
+
+//    console.log(" Serja Bem Vindo(a) ao Jogo de BlackJack!")
+    
+//if(confirm("Quer iniciar uma nova rodada?")){
+//console.log("Inicio de Jogo.")
+//let cartausuario1 = comprarCarta()
+//let cartausuario2 = comprarCarta()
+//let cartacomp1 = comprarCarta()
+//let cartacomp2 = comprarCarta()
+//let usuario1 = cartausuario1.valor 
+//let computador1 = cartacomp1.valor
+//let jogadausuario= usuario1 + usuario1
+//let jogadacomput = computador1 + computador1
+ 
+//if(jogadausuario > jogadacomput){
+  // console.log(`Usuario - carta: ${cartausuario2.texto} ${cartausuario2.texto} - Pontuação: ${usuario1}`)
+  // console.log(`Computador - carta: ${cartacomp2.texto} ${cartacomp2.texto} - Pontuação: ${computador1}`)
+  // console.log("O Usuario ganhou!")
+ //}else if(jogadausuario < jogadacomput){
+   //console.log(`Usuario - carta: ${cartausuario2.texto} ${cartausuario2.texto} - Pontuação: ${usuario1}`)
+   //console.log(`Computador - carta: ${cartacomp2.texto} ${cartacomp2.texto} - Pontuação: ${computador1}`)
+   //console.log("O computador ganhou!")
+ //}else if(jogadausuario === jogadacomput){
+   //console.log(`Usuario - carta: ${cartausuario2.texto} ${cartausuario2.texto} - Pontuação: ${usuario1}`)
+   //console.log(`Computador - carta: ${cartacomp2.texto} ${cartacomp2.texto} - Pontuação: ${computador1}`)
+   //console.log("Empate!")
+ //}
+//} else{
+   //console.log("O Jogo acabou.")
+//}
+
+
+   
+   
+   
+   
+   
+   
+   
+   
+
+
+
+
+
+
+
+
+   
+
+
+
+
