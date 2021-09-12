53a35dc7345e42583567d9a5a64055e4b59a6e71
laisMedeiros <medeiros_online@yahoo.com.br>
2021-09-12 20:16:44 -0300

    Quinzena5 aula18

--- quinzena5/Componentes-Funcionais-props/labedin/src/components/CardGrande/CardGrande.js
+++ quinzena5/Componentes-Funcionais-props/labedin/src/components/CardGrande/CardGrande.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './CardGrande.css'
 
 
@@ -6,7 +6,7 @@ function CardGrande(props) {
     return (
         <div className="bigcard-container">
             <img src={ props.imagem } />
-            
+         
             <div>
                 <h4>{ props.nome }</h4>
                 <p>{ props.descricao }</p>
