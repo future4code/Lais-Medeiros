ae8a5b4c86c09adbd7ace565fb697e3353537018
Lais de Medeiros Silva <medeiros_online@yahoo.com.br>
2021-10-07 17:15:07 -0300

    Alteracaoes

--- quinzena5/Renderizacao-de-Lista/insta-4/src/App.js
+++ quinzena5/Renderizacao-de-Lista/insta-4/src/App.js
@@ -1,44 +1,105 @@
 import React from 'react';
 import styled from 'styled-components'
 import Post from './components/Post/Post';
-import "./App.css"
 
-const AppConteiner = styled.div`
-display: flex;
-justify-content: center;
-flex-direction: column;
-align-items: center;
+const MainContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
 `
 
 class App extends React.Component {
-  state ={
-    postagem:[
+  state={
+    post:[
       {
-        nomeUsuario:'paulinha',
-        fotoUsuario: 'https://picsum.photos/50/50',
-        fotoPost:'https://picsum.photos/200/150'
-      }
-    ]
+          nomeUsuario:'Paulinha',
+          fotoUsuario:'https://picsum.photos/50/50',
+          fotoPost:'https://picsum.photos/200/150'
+
+      },
+      {
+         nomeUsuario:'Val',
+         fotoUsuario:'https://picsum.photos/50/51',
+         fotoPost:'https://picsum.photos/200/150'
+
+      },
+      {
+         nomeUsuario :'Eduardo',
+         fotoUsuario :'https://picsum.photos/50/52',
+         fotoPost: 'https://picsum.photos/200/150'
+
+  }
+    ],
+
+    valorNomeUsusario:"",
+    valorFotoUsusario:"",
+    valorFotoPost:""
+  }
+
+  adicionarPessoas = () =>{
+    const novosPost = {
+      nomeUsuario : this.state.valorNomeUsusario,
+      fotoUsuario : this.state.valorNomeUsusario,
+      fotoPost : this.state.valorFotoPost
+    }
+    const novosPosts = [...this.state.post, novosPost]
+    this.setState({ pessoas: novosPosts})
+  }
+
+  onChangeInputNome = (e) =>{
+    this.setState({valorNomeUsusario: e.target.value}
+      )
+  }
+  onChangeInputFoto = (e) =>{
+    this.setState({valorFotoUsusario: e.target.value}
+      )
+  }
+  onChangeInputPost = (e) =>{
+    this.setState({valorFotoPost: e.target.value}
+      )
   }
 
   render() {
-    const listaDePostagem = this.state.postagem.map((postar)=>{
+    const listaDePostagem = this.state.post.map((pessoa)=>{
       return(
-        <div className= "AppConteiner">
-          <p>
-        {postar.nomeUsuario}
-        {postar.fotoUsuario}
-        { postar.fotoPost}
+        <p>
+          {pessoa.nomeUsuario}
+          {pessoa.fotoUsuario}
+          {pessoa.fotoPost}
         </p>
-        
-        </div>
-        
       )
     })
     return (
-      <div>{listaDePostagem}</div>
+      <MainContainer>
+        <Post
+         
+        />
+        
+          <input 
+            value ={this.state.valorNomeUsusario}
+            onChange={this.onChangeInputNome}
+            placeholder={"Nome do Usuario"}
+         />
+          <input 
+            value ={this.state.valorFotoUsusario}
+            onChange={this.onChangeInputFoto}
+            placeholder={"Foto Usuario"}
+          />
+          <input 
+            value ={this.state.valorFotoPost}
+            onChange={this.onChangeInputPost}
+            placeholder={"Foto Post"}
+          />
+    
+           <button onClick = {this.adicionarPessoas}>Adicionar</button>
+            
+            {listaDePostagem}
+      </MainContainer>
     );
+    
   }
 }
 
 export default App;
+
